8648470e20cc12e0ff49e023f09d2f86d51b0b55
prathyusha-ux <prathyusha@uxinterfacely.com>
2026-09-11 20:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -380,7 +380,7 @@ function buildPage4(values, salary) {
       <div class="sign-line"><span class="lbl"><strong>Date</strong></span><span class="fill"></span></div>
     </div>
 
-    <p style="margin-top:24px;"><span style="color:#111;font-size:15px;">Regards</span><br><strong>${COMPANY.name}</strong></p>
+    <p style="margin-top:24px;"><span style="color:#111;font-size:15px;font-weight:5px;">Regards</span><br><strong>${COMPANY.name}</strong></p>
     <img class="stamp-img" src="${COMPANY.stampImage}" alt="Company stamp and signature">
   `, false);
 }
