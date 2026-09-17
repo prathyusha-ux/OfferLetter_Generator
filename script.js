581a0877c8717da6dc10bcd0d3369d2131d85a97
prathyusha-ux <prathyusha@uxinterfacely.com>
2026-09-17 11:50:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -297,7 +297,26 @@ function buildPage2(values, salary) {
     <p>The Company reserves the right to accept/ reject the notice pay in lieu of notice period mentioned here in above.</p>
     <p>The Company reserves the absolute right to terminate your services at any time, without assigning any reason whatsoever, by giving you notice in writing or pay in lieu of notice period.</p>
     <p>The Company reserves the right, at its discretion and at any time during the notice period to announce to employees, clients, suppliers and customers of the Company, its subsidiaries or associate companies of your termination/ resignation. However, you expressly agree hereby, not to make any announcement of your termination/ resignation, unless the same has been formally intimated to you or accepted by the Company in writing, as the case may be.</p>
+    <h3>Leave Entitlement and Policy</h3>
+    <p>For the purpose of leave entitlement, the leave year shall be considered as the calendar year commencing from 1 January and ending on 31 December of each year.</p>
+    <p>The Employee shall be entitled to a total of fifteen (15) days of leave per leave year, subject to the Company's prevailing Leave Policy and applicable rules. The annual leave entitlement shall consist of:</p>
+    <ul>
+      <li>Casual Leave (CL): 10 days per leave year</li>
+      <li>Sick Leave (SL): 5 days per leave year</li>
+    </ul>
+    <p>Leave shall be availed with the prior approval of the reporting manager or the authorized person, except in cases of emergency or sudden illness, where the Employee shall inform the Company at the earliest possible opportunity.</p>
+    <p>Employees joining during the course of a leave year shall be eligible for leave on a pro-rata basis, as applicable under the Company's Leave Policy.</p>
+    <p>The carry-forward, lapse, or encashment of unused leave shall be governed by the Company's prevailing policies and applicable statutory requirements.</p>
+    <p>The Company reserves the right to amend or modify the Leave Policy from time to time in accordance with business requirements and applicable laws.</p>
 
+    <h3>PERFORMANCE BASED VARIABLE BONUS/ INCENTIVE:</h3>
+    <p>You will be eligible to participate in the Company's variable pay programs/ incentive schemes. The payment under this program depends on your performance, the Company's performance and other parameters as the Company may decide from time to time. Please note that there is no minimum payment under this program.</p>
+    <p>Payment of this amount is subject to your being in the Company's employment and also subject to your not having resigned or serving your notice period.</p>
+    <p>You will declare your relationship, if any, with any of the directors of the Company as required by the Companies Act, 2013.</p>
+    <p>In case you are or become related to any employee of the Company, then, in the former case you will inform the Company immediately and in the latter case within 7 days of your becoming so.</p>
+    <p>You will abide by all the policies and disclosure norms of the Company that are in effect and by any amendments thereto carried out by the Company from time to time.</p>
+  `, false);
+}
     <h3>PERFORMANCE BASED VARIABLE BONUS/ INCENTIVE:</h3>
     <p>You will be eligible to participate in the Company's variable pay programs/ incentive schemes. The payment under this program depends on your performance, the Company's performance and other parameters as the Company may decide from time to time. Please note that there is no minimum payment under this program.</p>
     <p>Payment of this amount is subject to your being in the Company's employment and also subject to your not having resigned or serving your notice period.</p>
